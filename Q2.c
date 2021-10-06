84d1bdb942f3d36d222dbe139f3796c27862de16
Kazan <andyswebking@gmail.com>
2021-10-06 17:28:08 +0800

    10/04 OJ Q2

--- Q2.c
+++ Q2.c
@@ -4,28 +4,30 @@ int main(void) {
     int grade;
     scanf("%d", &grade);
 
-    if (grade >= 60 && grade <= 69) {
-        if (grade >= 70 && grade <= 79){
-            if (grade >= 80 && grade <= 89) {
-                if (grade >= 90 && grade <= 100){
-                    printf("%s", "A\n");
+    if(grade >= 0) {
+        if(grade >= 60) {
+            if(grade >= 70) {
+                if(grade >= 80) {
+                    if(grade >= 90 && grade <= 100) {
+                        printf("%s", "A\n");
+                    }
+                    else {
+                        printf("%s", "B\n");
+                    }
                 }
                 else {
-                    printf("%s", "B\n");
+                    printf("%s", "C\n");
                 }
             }
             else {
-                printf("%s", "C\n");
+                printf("%s", "D\n");
             }
         }
         else {
-            printf("%s", "D\n");
-        }       
-    }
-    else {
-        printf("%s", "F\n");
+            printf("%s", "F\n");
+        }
     }
 
     return 0;
-    
+
 }
