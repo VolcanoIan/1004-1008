beb173dd5105e17312aa9fdf93360d8f90820558
Kazan <andyswebking@gmail.com>
2021-10-07 01:18:04 +0800

    10/04 OJ Q4

--- Q4.c
+++ Q4.c
@@ -11,24 +11,44 @@ int main(void) {
     else if ((year % 4 == 0) && (year % 100 == 0)) {
         if (year % 400 != 0) {
             printf("平年\n");
-            printf("離 %d 最近的閏年是 ％d 年或 %d 年\n", year, (year - 4), (year + 4));
+            printf("離 %d 最近的閏年是 %d 年或 %d 年\n", year, (year - 4), (year + 4));
         }
         else {
             printf("閏年\n");
         }
     }
     else {
-        if (n < 2) {
+        if (n == 1) {
             printf("平年\n");
-            printf("離 %d 最近的閏年是 %d 年\n", year, (year - n));
+            if ((year - 1) % 100 != 0) {
+                printf("離 %d 最近的閏年是 %d 年\n", year, (year - 1));
+            }
+            else {
+                printf("離 %d 最近的閏年是 %d 年\n", year, (year + 3));
+            }
         }
-        else if(n > 2) {
+        else if(n == 3) {
             printf("平年\n");
-            printf("離 %d 最近的閏年是 %d 年\n", year, (year + 4 - n));
+            if ((year + 1) % 100 != 0) {
+                printf("離 %d 最近的閏年是 %d 年\n", year, (year + 1));
+            }
+            else {
+                printf("離 %d 最近的閏年是 %d 年\n", year, (year - 3));
+            }
         }
         else if (n == 2) {
             printf("平年\n");
-            printf("離 %d 最近的閏年是 %d 年或 %d 年\n", year, (year -2), (year + 2));
+            if ((year + 2) % 100 == 0 || (year - 2) % 100 == 0) {
+                if ((year + 2) % 100 == 0) {
+                    printf("離 %d 年最近的閏年是 %d 年\n", year, year - 2);
+                }
+                else if ((year - 2) % 100 == 0) {
+                    printf("離 %d 年最近的閏年是 %d 年\n", year, year + 2);
+                }
+            }
+            else {
+                printf("離 %d 最近的閏年是 %d 年或 %d 年\n", year, (year - 2), (year + 2));
+            }
         }
     }
 }
