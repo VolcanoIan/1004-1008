6f2e05a48eba2df5ee610ce61a46a149f6d1a30b
Kazan <andyswebking@gmail.com>
2021-10-08 16:52:28 +0800

    10/06 OJ Q3

--- 1006Q3.c
+++ 1006Q3.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-    int n, i = 1, j =1;
+    int n, i = 0, j = 1;
     scanf("%d", &n);
 
-    while (i <= n) {
-        while (j <= n)
+    while (i < n) {
+        while (j < n)
         {
             printf("%s", "#");
             j++;
         }
+        j = 1;
         printf("%s", "#");
+        printf("%s", "\n");
         i++;
     }
     printf("%s", "\n");
